9007b3ed9346dc671072bf39bdbd0089c43a428a
AlexisRenchon <a.renchon@gmail.com>
2023-08-16 10:58:35 -0700

    I think it works

--- experiments/web_dashboards/dashboard.jl
+++ experiments/web_dashboards/dashboard.jl
@@ -4,35 +4,46 @@ using WGLMakie
 
 include("ozark.jl")
 include("ozark_met_drivers_FLUXNET.jl");
-include("ozark_domain.jl");
-include("ozark_parameters.jl");
-include("ozark_simulation.jl");
+function init() # not sure why needed to rerun each time before using ClimaLSM_ozark(), otherwise return Any[]
+  include("ozark_domain.jl");
+  include("ozark_parameters.jl");
+  include("ozark_simulation.jl");
+end
 
-GPP = ClimaLSM_ozark(141.0) .* 1e6 # test - works on 1st call, but subsequent calls return Any[]
+init();
+GPP = ClimaLSM_ozark(200.0) .* 1e6 # need init() first, each time
 
 fig = Figure(); display(fig) # should be in ClimaLSM_dashboard, but bug for some reason
 
 function ClimaLSM_dashboard(button, g1_input)
   ax = Axis(fig[1,1], xlabel = "day", ylabel = "GPP")
   daily = collect(range(120, 140, 481)) # could be output from ClimaLSM_ozark()
-  #GPP = Observable(ones(481))
+  GPP = Observable(ones(481))
+  pdata = @lift(Vec2f.(daily, $GPP))
+  plt = lines!(ax, pdata)
+  g1 = g1_input.value 
+
+
+
+  # init(); # This is needed, but error when run inside a function (world age error)
+
+
+
+  #GPP = @lift(ClimaLSM_ozark($g1))
   #pdata = @lift(Vec2f.(daily, $GPP))
   #plt = lines!(ax, pdata)
 
-  g1 = g1_input.value  
-  GPP = @lift(ClimaLSM_ozark($g1)) # empty array...
-  pdata = @lift(Vec2f.(daily, $GPP))
-  plt = lines!(ax, pdata)
-
   on(button) do click
-    g1 = g1_input.value
+    g1[] = g1_input.value[]
     # sv[] = 
     # probably need to work with g1 as an observable
     # GPP[] = [parent(sv.saveval[k].canopy.photosynthesis.GPP)[1] for k in 1:length(sv.saveval)]   
     # GPP[] = ClimaLSM_ozark(g1)
     
-    GPP = @lift(ClimaLSM_ozark($g1))
+    init();
+    GPP[] = @lift(ClimaLSM_ozark($g1))[]
     autolimits!(ax)
+    #fig
   end
 
   fig
