697fe287f7655ea4e28dfb4bb873652b883d7bb0
Alexis Renchon <a.renchon@gmail.com>
2024-01-02 16:29:28 -0800

    unitful SI or commonly used

--- experiments/integrated/fluxnet/inputs_dataframe.jl
+++ experiments/integrated/fluxnet/inputs_dataframe.jl
@@ -1,7 +1,8 @@
 #= run those lines for testing / development
+
 ARGS = ["US-MOz"]
 include("integrated/fluxnet/setup.jl")
-include("integrated/fluxnet/run_fluxnet.jl")
+
 =#
 
 using DataFrames
@@ -50,3 +51,21 @@ variables = [
 inputs = DataFrame([
     variables_name[i] => variables[i] for i in 1:length(variables_name)
 ])
+
+# make a Unitful dataframe with SI units and one with commonly used units
+
+using Unitful: R, L, mol, K, kJ, °C, m, g, cm, hr, mg, s, μmol, Pa, W, mm
+using UnitfulMoles: molC
+using Unitful, UnitfulMoles
+@compound CO₂
+
+columns = variables_name[2:end]
+units = [molCO₂*m^-2*s^-1, K, Pa, Pa, m*s^-1, m*s^-1, W*m^-2, W*m^-2, molCO₂, m^3*m^-3, K, molCO₂*m^-2*s^-1, W*m^-2, W*m^-2, W*m^-2, W*m^-2]
+inputs_SI = copy(inputs)
+foreach((col, unit) -> inputs_SI[!, col] .= first([inputs_SI[!, col]]unit), columns, units)
+
+units_to = [μmolCO₂*m^-2*s^-1, °C, kPa, Pa, mm*s^-1, m*s^-1, W*m^-2, W*m^-2, μmolCO₂, m^3*m^-3, °C, μmolCO₂*m^-2*s^-1, W*m^-2, W*m^-2, W*m^-2, W*m^-2]
+inputs_commonly_used = copy(inputs_SI)
+foreach((col, unit_to) -> inputs_commonly_used[!, col] = uconvert.(unit_to, inputs_SI[!, col]), columns, units_to)
+
+
