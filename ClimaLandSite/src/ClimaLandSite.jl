ca4bc816dd3ed7a46246c83f9a80f222869c75ac
Alexis Renchon <a.renchon@gmail.com>
2024-01-04 09:38:15 -0800

    WIP

--- ClimaLandSite/src/ClimaLandSite.jl
+++ ClimaLandSite/src/ClimaLandSite.jl
@@ -47,15 +47,32 @@ include(joinpath(climalsm_dir, "parameters", "create_parameters.jl"))
 earth_param_set = create_lsm_parameters(FT)
 
 include("run.jl")
-include(joinpath("utilities", "climalsm_output_dataframe.jl"))
-include(joinpath("utilities", "data_tools.jl"))
-include(joinpath("utilities", "fluxnet_domain.jl"))
-include(joinpath("utilities", "fluxnet_simulation.jl"))
-include(joinpath("utilities", "inputs_dataframe.jl"))
-include(joinpath("utilities", "makie_plots.jl"))
-include(joinpath("utilities", "met_drivers_FLUXNET.jl"))
-include(joinpath("utilities", "pull_MODIS.jl"))
 export run
+
+include(joinpath("utilities", "climalsm_output_dataframe.jl"))
+export getoutput
+
+include(joinpath("utilities", "data_tools.jl"))
+export replace_missing_with_mean!, replace_missing_with_mean_by_value!, check_column, DataColumn,
+       VerifiedColumn, transform_column
+
+include(joinpath("utilities", "fluxnet_domain.jl"))
+# need to create methods
+
+include(joinpath("utilities", "fluxnet_simulation.jl"))
+# need to create methods
+
+include(joinpath("utilities", "inputs_dataframe.jl"))
+# need to create methods
+
+include(joinpath("utilities", "makie_plots.jl"))
+export timeseries_fluxes_fig, timeseries_H2O_fig, fingerprint_fig, diurnal, diurnal_plot!, diurnals_fig
+
+include(joinpath("utilities", "met_drivers_FLUXNET.jl"))
+export zenith_angle
+
+include(joinpath("utilities", "pull_MODIS.jl"))
+export send_get_subset, check_response, parse_response, single_col_data_matrix
 
 function __init__()
     Unitful.register(ClimaLandSite)
