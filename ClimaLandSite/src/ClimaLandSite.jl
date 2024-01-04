f6697e776b00cb03b23811118a1de527890bf265
Alexis Renchon <a.renchon@gmail.com>
2024-01-04 09:18:42 -0800

    WIP

--- ClimaLandSite/src/ClimaLandSite.jl
+++ ClimaLandSite/src/ClimaLandSite.jl
@@ -1,5 +1,29 @@
 module ClimaLandSite
 
-using ClimaLSM # rename to ClimaLand WIP
+import SciMLBase
+import ClimaTimeSteppers as CTS
+using ClimaCore
+import CLIMAParameters as CP
+using Statistics
+using Dates
+using Insolation
+using StatsBase
+using Interpolations
+using StatsBase
+
+using ClimaLSM
+using ClimaLSM.Domains: Column
+using ClimaLSM.Soil
+using ClimaLSM.Soil.Biogeochemistry
+using ClimaLSM.Canopy
+using ClimaLSM.Canopy.PlantHydraulics
+import ClimaLSM
+import ClimaLSM.Parameters as LSMP
+
+climalsm_dir = pkgdir(ClimaLSM)
+include(joinpath(climalsm_dir, "parameters", "create_parameters.jl"))
+
+const FT = Float64
+earth_param_set = create_lsm_parameters(FT)
 
 end
